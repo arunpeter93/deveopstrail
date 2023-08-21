6fc12314ff70160c564c37acdce587afb9c09b81
root <root@ip-172-31-5-36.us-east-2.compute.internal>
2023-08-21 15:37:45 +0000

    This is the third commit

--- arun.py
+++ arun.py
@@ -1,4 +1,4 @@
-print('Hi,,,,,,,,,,This is the second commit,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+print('Hi,,,,,,,,,,This is the Third commit,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
       What are you doing')
 for i in range(1,70):
-    print(i)
+    print(i*'*')
